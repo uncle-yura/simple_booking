3bdbefe5a28e5af39a2e41df358ef51e737f8bcf
uncle-yura <uncle-yura@tuta.io>
2021-07-25 17:29:53 +0300

    Changed user name display function.

--- base/models.py
+++ base/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+User.__str__ = lambda user_instance: user_instance.first_name + " " + user_instance.last_name if user_instance.first_name and user_instance.last_name else user_instance.username
